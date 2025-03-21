54ebc8add15f0f55871991b51530227d4abf6498
Anupam Chopra <anupamchopra@Anupams-MacBook-Air.local>
2025-03-21 11:34:05 -0300

    For check

--- src/main/java/com/example/container1/Container1Application.java
+++ src/main/java/com/example/container1/Container1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Container1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Container1Application.class, args); // trigger //
+		SpringApplication.run(Container1Application.class, args); // trigger // works
 	}
 
 }
